226c10791be9e41166598ed2b4a35330ab9ee91e
elbillyto <elbillyto@gmail.com>
2015-05-04 16:19:41 +0200

    codigo traido de cloud9
    https://ide.c9.io/elbillyto/demo-project#openfile-README
    que a su vez se basa en http://www.tutorialspoint.com/python/python_exceptions.htm

--- Tutopython/test.py
+++ Tutopython/test.py
@@ -1,4 +1,6 @@
-#!/usr/bin/python
+# this is the simplest python program
+
+print ("Hello world from Cloud9!")
 def KelvinToFahrenheit(Temperature):
     assert (Temperature >= 0),"Colder than absolute zero!"
     return ((Temperature-273)*1.8)+32
@@ -14,3 +16,29 @@ except IOError:
 else:
    print "Written content in the file successfully"
    fh.close()
+   
+try:
+   fh = open("testfile", "r")
+   fh.write("This is my test file for exception handling!!")
+except IOError:
+   print "Error: can\'t find file or read data"
+else:
+   print "Written content in the file successfully"
+# click the 'Run' button at the top to start this application
+
+
+try:
+   fh = open("testfile", "w")
+   fh.write("This is my test file for exception handling!!")
+finally:
+   print "Error: finally can\'t find file or read data"
+   
+try:
+   fh = open("testfile", "w")
+   try:
+      fh.write("This is my test file for exception handling!!")
+   finally:
+      print "finally Going to close the file"
+      fh.close()
+except IOError:
+   print "Error: can\'t find file or read data"
