f66c6d8302eb38fcb11dda5f6da72e11a3e46ecd
elbillyto <elbillyto@gmail.com>
2015-05-04 16:45:55 +0200

    traido de cloud9
    conforme a  http://www.tutorialspoint.com/python/python_reg_expressions.htm

--- Tutopython/test.py
+++ Tutopython/test.py
@@ -1,3 +1,5 @@
+import re
+
 # this is the simplest python program
 
 print ("Hello world from Cloud9!")
@@ -42,3 +44,181 @@ try:
       fh.close()
 except IOError:
    print "Error: can\'t find file or read data"
+   
+# Define a function here.
+def temp_convert(var):
+   try:
+      return int(var)
+   except ValueError, Argument:
+      print "The argument does not contain numbers\n", Argument
+
+# Call above function here.
+temp_convert("xyz");
+
+def functionName( level ):
+   if level < 1:
+      raise ValueError("Invalid level!", level)
+      # The code below to this would not be executed
+      # if we raise the exception
+      
+# creo mi clase de excepcion
+class Networkerror(RuntimeError):
+   def __init__(self, arg):
+      self.args = arg
+try:
+   raise Networkerror("Bad hostname")
+except Networkerror,e:
+   print e.args
+
+
+#CREACION DE CLASES
+class Employee:
+   'Common base class for all employees'
+   empCount = 0
+
+   def __init__(self, name, salary):
+      self.name = name
+      self.salary = salary
+      Employee.empCount += 1
+   
+   def displayCount(self):
+     print "Total Employee %d" % Employee.empCount
+
+   def displayEmployee(self):
+      print "Name : ", self.name,  ", Salary: ", self.salary
+      
+"This would create first object of Employee class"
+emp1 = Employee("Zara", 2000)
+"This would create second object of Employee class"
+emp2 = Employee("Manni", 5000)
+emp1.displayEmployee()
+emp2.displayEmployee()
+print "Total Employee %d" % Employee.empCount
+#modifio objetos
+emp1.age = 7  # Add an 'age' attribute.
+emp1.age = 8  # Modify 'age' attribute.
+del emp1.age  # Delete 'age' attribute.
+
+#hasattr(emp1, 'age')    # Returns true if 'age' attribute exists
+#getattr(emp1, 'age')    # Returns value of 'age' attribute
+#setattr(emp1, 'age', 8) # Set attribute 'age' at 8
+#delattr(emp1, 'age')    # Delete attribute 'age'
+
+print "Employee.__doc__:", Employee.__doc__
+print "Employee.__name__:", Employee.__name__
+print "Employee.__module__:", Employee.__module__
+print "Employee.__bases__:", Employee.__bases__
+print "Employee.__dict__:", Employee.__dict__
+
+
+## DESTROYING OBJECTS
+a = 40      # Create object <40>
+b = a       # Increase ref. count  of <40> 
+c = [b]     # Increase ref. count  of <40> 
+
+del a       # Decrease ref. count  of <40>
+b = 100     # Decrease ref. count  of <40> 
+c[0] = -1   # Decrease ref. count  of <40> 
+class Point:
+   def __init( self, x=0, y=0):
+      self.x = x
+      self.y = y
+   def __del__(self):
+      class_name = self.__class__.__name__
+      print class_name, "destroyed"
+
+pt1 = Point()
+pt2 = pt1
+pt3 = pt1
+print id(pt1), id(pt2), id(pt3) # prints the ids of the obejcts
+del pt1
+del pt2
+del pt3
+
+
+## HERENCIA
+class Parent:        # define parent class
+   parentAttr = 100
+   def __init__(self):
+      print "Calling parent constructor"
+
+   def parentMethod(self):
+      print 'Calling parent method'
+
+   def setAttr(self, attr):
+      Parent.parentAttr = attr
+
+   def getAttr(self):
+      print "Parent attribute :", Parent.parentAttr
+
+class Child(Parent): # define child class
+   def __init__(self):
+      print "Calling child constructor"
+
+   def childMethod(self):
+      print 'Calling child method'
+
+c = Child()          # instance of child
+c.childMethod()      # child calls its method
+c.parentMethod()     # calls parent's method
+c.setAttr(200)       # again call parent's method
+c.getAttr()          # again call parent's method
+
+##Overriding Methods
+print "\nOverriding Methods"
+class Padre:        # define parent class
+   def myMethod(self):
+      print 'Calling parent method'
+
+class Hijo(Parent): # define child class
+   def myMethod(self):
+      print 'Calling child method'
+
+c = Hijo()          # instance of child
+c.myMethod()         # child calls overridden method
+
+##Overloading Operators
+print "\nOverloading Operators"
+class Vector:
+   def __init__(self, a, b):
+      self.a = a
+      self.b = b
+
+   def __str__(self):
+      return 'Vector (%d, %d)' % (self.a, self.b)
+   
+   def __add__(self,other):
+      return Vector(self.a + other.a, self.b + other.b)
+
+v1 = Vector(2,10)
+v2 = Vector(5,-2)
+print v1 + v2
+
+##Data Hiding
+print "\nData Hiding"
+class JustCounter:
+   __secretCount = 0
+  
+   def count(self):
+      self.__secretCount += 1
+      print self.__secretCount
+
+counter = JustCounter()
+counter.count()
+counter.count()
+#print counter.__secretCount
+#print counter._JustCounter__secretCount
+
+##The match Function
+print "\nThe match Function"
+
+line = "Cats are smarter than dogs"
+
+matchObj = re.match( r'(.*) are (.*?) .*', line, re.M|re.I)
+
+if matchObj:
+   print "matchObj.group() : ", matchObj.group()
+   print "matchObj.group(1) : ", matchObj.group(1)
+   print "matchObj.group(2) : ", matchObj.group(2)
+else:
+   print "No match!!"
