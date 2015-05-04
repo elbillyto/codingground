7c63f0380e3f28611e7006ea6589d1b0871ae629
manuelisbank@gmail.com <manuelisbank@gmail.com>
2015-05-04 17:43:30 +0200

    Borro carpeta New Project porque Tutopython es su evolución

--- New Project/Phone/G3.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python
-
-def Pots():
-   print "I'm Pots Phone"
-
-def G3():
-   print "I'm G3 Phone"   
--- New Project/Phone/Isdn.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python
-
-def Pots():
-   print "I'm Pots Phone"
-   
-def Isdn():
-   print "I'm Isdn  Phone"
