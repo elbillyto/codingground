f898f5f3060fa900d44bb3ebecf20005a4febf8b
elbillyto <elbillyto@gmail.com>
2015-05-06 00:02:21 +0200

    *Cambios cosmeticos de formato basados en Pycharm *Añado gitignore ignorando ficheros .idea de Pycharm

--- cloud9/hello.py
+++ cloud9/hello.py
@@ -1,14 +1,18 @@
+# coding=utf-8
 import re
 
 # this is the simplest python program
 
 print ("Hello world from Cloud9!")
+
+
 def KelvinToFahrenheit(Temperature):
-    assert (Temperature >= 0),"Colder than absolute zero!"
-    return ((Temperature-273)*1.8)+32
+    assert (Temperature >= 0), "Colder than absolute zero!"
+    return ((Temperature - 273) * 1.8) + 32
+
 print KelvinToFahrenheit(273)
 print int(KelvinToFahrenheit(505.78))
-#print KelvinToFahrenheit(-5)
+# print KelvinToFahrenheit(-5)
 
 try:
    fh = open("testfile", "w")
@@ -18,7 +22,7 @@ except IOError:
 else:
    print "Written content in the file successfully"
    fh.close()
-   
+
 try:
    fh = open("testfile", "r")
    fh.write("This is my test file for exception handling!!")
@@ -26,15 +30,13 @@ except IOError:
    print "Error: can\'t find file or read data"
 else:
    print "Written content in the file successfully"
-# click the 'Run' button at the top to start this application
-
 
 try:
    fh = open("testfile", "w")
    fh.write("This is my test file for exception handling!!")
 finally:
    print "Error: finally can\'t find file or read data"
-   
+
 try:
    fh = open("testfile", "w")
    try:
@@ -44,7 +46,8 @@ try:
       fh.close()
 except IOError:
    print "Error: can\'t find file or read data"
-   
+
+
 # Define a function here.
 def temp_convert(var):
    try:
@@ -55,38 +58,43 @@ def temp_convert(var):
 # Call above function here.
 temp_convert("xyz");
 
-def functionName( level ):
+
+def functionName(level):
    if level < 1:
       raise ValueError("Invalid level!", level)
       # The code below to this would not be executed
       # if we raise the exception
-      
+
+
 # creo mi clase de excepcion
 class Networkerror(RuntimeError):
    def __init__(self, arg):
       self.args = arg
+
+
 try:
    raise Networkerror("Bad hostname")
-except Networkerror,e:
+except Networkerror, e:
    print e.args
 
 
-#CREACION DE CLASES
+# CREACION DE CLASES
 class Employee:
-   'Common base class for all employees'
-   empCount = 0
+    'Clase base común para todos los empleados'
+    empCount = 0
 
-   def __init__(self, name, salary):
-      self.name = name
-      self.salary = salary
-      Employee.empCount += 1
-   
-   def displayCount(self):
-     print "Total Employee %d" % Employee.empCount
+    def __init__(self, name, salary):
+       self.name = name
+       self.salary = salary
+       Employee.empCount += 1
 
-   def displayEmployee(self):
-      print "Name : ", self.name,  ", Salary: ", self.salary
-      
+    def displayCount(self):
+       print "Total Employee %d" % Employee.empCount
+
+    def displayEmployee(self):
+       print "Name : ", self.name, ", Salary: ", self.salary
+
+
 "This would create first object of Employee class"
 emp1 = Employee("Zara", 2000)
 "This would create second object of Employee class"
@@ -94,12 +102,12 @@ emp2 = Employee("Manni", 5000)
 emp1.displayEmployee()
 emp2.displayEmployee()
 print "Total Employee %d" % Employee.empCount
-#modifio objetos
+# modifico objetos
 emp1.age = 7  # Add an 'age' attribute.
 emp1.age = 8  # Modify 'age' attribute.
 del emp1.age  # Delete 'age' attribute.
 
-#hasattr(emp1, 'age')    # Returns true if 'age' attribute exists
+# hasattr(emp1, 'age')    # Returns true if 'age' attribute exists
 #getattr(emp1, 'age')    # Returns value of 'age' attribute
 #setattr(emp1, 'age', 8) # Set attribute 'age' at 8
 #delattr(emp1, 'age')    # Delete attribute 'age'
@@ -112,70 +120,79 @@ print "Employee.__dict__:", Employee.__dict__
 
 
 ## DESTROYING OBJECTS
-a = 40      # Create object <40>
-b = a       # Increase ref. count  of <40> 
-c = [b]     # Increase ref. count  of <40> 
+a = 40  # Create object <40>
+b = a  # Increase ref. count  of <40>
+c = [b]  # Increase ref. count  of <40>
 
-del a       # Decrease ref. count  of <40>
-b = 100     # Decrease ref. count  of <40> 
-c[0] = -1   # Decrease ref. count  of <40> 
+del a  # Decrease ref. count  of <40>
+b = 100  # Decrease ref. count  of <40>
+c[0] = -1  # Decrease ref. count  of <40>
+
+
 class Point:
-   def __init( self, x=0, y=0):
+   def __init(self, x=0, y=0):
       self.x = x
       self.y = y
+
    def __del__(self):
       class_name = self.__class__.__name__
       print class_name, "destroyed"
 
+
 pt1 = Point()
 pt2 = pt1
 pt3 = pt1
-print id(pt1), id(pt2), id(pt3) # prints the ids of the obejcts
+print id(pt1), id(pt2), id(pt3)  # prints the ids of the obejcts
 del pt1
 del pt2
 del pt3
 
 
 ## HERENCIA
-class Parent:        # define parent class
+class Parent:  # define parent class
    parentAttr = 100
+
    def __init__(self):
-      print "Calling parent constructor"
+      print "Constructor padre"
 
    def parentMethod(self):
-      print 'Calling parent method'
+      print 'método padre'
 
    def setAttr(self, attr):
       Parent.parentAttr = attr
 
    def getAttr(self):
-      print "Parent attribute :", Parent.parentAttr
+      print "atributo padre :", Parent.parentAttr
 
-class Child(Parent): # define child class
+
+class Child(Parent):  # define child class
    def __init__(self):
-      print "Calling child constructor"
+      print "Constructor Hijo"
 
    def childMethod(self):
-      print 'Calling child method'
+      print 'method hijo'
 
-c = Child()          # instance of child
-c.childMethod()      # child calls its method
-c.parentMethod()     # calls parent's method
-c.setAttr(200)       # again call parent's method
-c.getAttr()          # again call parent's method
+
+c = Child()  # instance of child
+c.childMethod()  # child calls its method
+c.parentMethod()  # calls parent's method
+c.setAttr(200)  # again call parent's method
+c.getAttr()  # again call parent's method
 
 ##Overriding Methods
 print "\nOverriding Methods"
-class Padre:        # define parent class
+class Padre:  # define parent class
    def myMethod(self):
       print 'Calling parent method'
 
-class Hijo(Parent): # define child class
+
+class Hijo(Parent):  # define child class
    def myMethod(self):
       print 'Calling child method'
 
-c = Hijo()          # instance of child
-c.myMethod()         # child calls overridden method
+
+c = Hijo()  # instance of child
+c.myMethod()  # child calls overridden method
 
 ##Overloading Operators
 print "\nOverloading Operators"
@@ -186,22 +203,24 @@ class Vector:
 
    def __str__(self):
       return 'Vector (%d, %d)' % (self.a, self.b)
-   
-   def __add__(self,other):
+
+   def __add__(self, other):
       return Vector(self.a + other.a, self.b + other.b)
 
-v1 = Vector(2,10)
-v2 = Vector(5,-2)
+
+v1 = Vector(2, 10)
+v2 = Vector(5, -2)
 print v1 + v2
 
 ##Data Hiding
 print "\nData Hiding"
 class JustCounter:
    __secretCount = 0
-  
+
    def count(self):
       self.__secretCount += 1
       print self.__secretCount
+
 
 counter = JustCounter()
 counter.count()
@@ -211,10 +230,8 @@ counter.count()
 
 ##The match Function
 print "\nThe match Function"
-
 line = "Cats are smarter than dogs"
-
-matchObj = re.match( r'(.*) are (.*?) .*', line, re.M|re.I)
+matchObj = re.match(r'(.*) are (.*?) .*', line, re.M | re.I)
 
 if matchObj:
    print "matchObj.group() : ", matchObj.group()
